550c5fc71cf7dd6300b3d0692cb99eea363c7e59
Mia <miasuffee001@gmail.com>
2025-09-20 21:45:08 +0100

    user

--- app/routes/auth/utils.py
+++ app/routes/auth/utils.py
@@ -16,3 +16,16 @@ async def _get_verification_code(payload: EmailVerification, db: AsyncSession = 
 @router.post("/verify/email/")
 async def _verify_email(payload: EmailVerification, db: AsyncSession = Depends(get_async_db)):
     return await verify_email(db, payload.email, payload.verification_code)
+
+@router.post("/reset/password/")
+async def _reset_admin_password(payload:PasswordResetPayloadRequest, db: AsyncSession = Depends(get_async_db)):
+    return await request_password_reset(db, payload.email, payload.unique_id)
+
+@router.post("/otp")
+async def _verify_reset_code(payload: EmailVerification, db: AsyncSession = Depends(get_async_db)):
+    return await verify_reset_code(db, payload.email, payload.verification_code)
+
+
+@router.post("/reset/with/otp/")
+async def _reset_with_otp(payload: ResetOtp, db: AsyncSession = Depends(get_async_db)):
+    return await reset_password_with_otp(db, payload.email, payload.otp, payload.password)
